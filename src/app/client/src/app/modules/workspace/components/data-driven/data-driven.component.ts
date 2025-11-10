f99e1298e5e31320c729adfc5cba4c6d55eb2dd5
Akshitha <akshitha@coss.org.in>
2025-11-10 15:31:39 +0530

    changed forEach

--- src/app/client/src/app/modules/workspace/components/data-driven/data-driven.component.ts
+++ src/app/client/src/app/modules/workspace/components/data-driven/data-driven.component.ts
@@ -329,20 +329,30 @@ export class DataDrivenComponent extends WorkSpace implements OnInit, OnDestroy,
     } else {
       requestData.contentType = this.configService.appConfig.contentCreateTypeForEditors[this.contentType];
     }
-   
-    this.formFieldProperties.forEach(field => {
-    const code = field.code;
-    const dataType = field.dataType;
-    const categoryValue = requestData[code];
-      if (_.isUndefined(categoryValue)) return;
-      if (dataType === 'text' && !_.isArray(categoryValue)) {
-        requestData[code] = categoryValue.toString(); 
-      }
-      else if (dataType === 'list' && !_.isArray(categoryValue)) {
+
+    for(const {code, dataType} of this.formFieldProperties) {
+      const categoryValue = requestData[code];
+      if (_.isUndefined(categoryValue)) continue;
+      if (dataType === 'list' && !_.isArray(categoryValue)) {
         requestData[code] = [categoryValue];
       }
-    });
-    console.log("Final request data", requestData);
+    }
+    
+    console.log("requestData", requestData);
+   
+    // this.formFieldProperties.forEach(field => {
+    // const code = field.code;
+    // const dataType = field.dataType;
+    // const categoryValue = requestData[code];
+    //   if (_.isUndefined(categoryValue)) return;
+    //   if (dataType === 'text' && !_.isArray(categoryValue)) {
+    //     requestData[code] = categoryValue.toString(); 
+    //   }
+    //   else if (dataType === 'list' && !_.isArray(categoryValue)) {
+    //     requestData[code] = [categoryValue];
+    //   }
+    // });
+    // console.log("Final request data", requestData);
     return requestData;
   }
 
